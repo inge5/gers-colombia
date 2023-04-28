0c8df68a626e15c1ba9a7bb7048f3f2d4dc9f4b9
Nelson Zambrano <inge5neuromedia@gmail.com>
2023-04-28 17:57:00 -0500

    cambio en pensamiento corporativo

--- src/app/colombia/nosotros/pensamiento-corporativo/pensamiento-corporativo.component.ts
+++ src/app/colombia/nosotros/pensamiento-corporativo/pensamiento-corporativo.component.ts
@@ -2,14 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PagesService } from '../../../services/pages.service';
 
-
 @Component({
   selector: 'app-pensamiento-corporativo',
   templateUrl: './pensamiento-corporativo.component.html',
-  styleUrls: ['./pensamiento-corporativo.component.css']
+  styleUrls: ['./pensamiento-corporativo.component.css'],
 })
 export class PensamientoCorporativoComponent implements OnInit {
-
   proposito_empresa_data: any[] = [];
   valor_corporativo_columna1_data: any[] = [];
   valor_corporativo_columna2_data: any[] = [];
@@ -20,28 +18,33 @@ export class PensamientoCorporativoComponent implements OnInit {
   titulo_pilares_data: any;
   pilar_data: any[] = [];
   mapa_procesos_data: any;
-
+  quienes_somos: string = '';
 
   loader = true;
 
-  constructor(private httpClient:HttpClient, private _pensamientocorporativo:PagesService) { }
+  constructor(
+    private httpClient: HttpClient,
+    private _pensamientocorporativo: PagesService
+  ) {}
 
   ngOnInit(): void {
-    this._pensamientocorporativo.getPensamientoCorporativo()
-      .subscribe((res:any) => {
+    this._pensamientocorporativo
+      .getPensamientoCorporativo()
+      .subscribe((res: any) => {
         this.loader = false;
         this.proposito_empresa_data = res.acf.proposito_empresa;
         this.imagen_proposito_data = res.acf.imagen_proposito;
         this.titulo_valores_data = res.acf.titulo_valores;
-        this.valor_corporativo_columna1_data = res.acf.valor_corporativo_columna1;
-        this.valor_corporativo_columna2_data = res.acf.valor_corporativo_columna2;
-        this.seccion_compromisos_data= res.acf.seccion_compromisos;
-        this.seccion_propuesta_data= res.acf.seccion_propuesta;
-        this.titulo_pilares_data= res.acf.titulo_pilares;
+        this.valor_corporativo_columna1_data =
+          res.acf.valor_corporativo_columna1;
+        this.valor_corporativo_columna2_data =
+          res.acf.valor_corporativo_columna2;
+        this.seccion_compromisos_data = res.acf.seccion_compromisos;
+        this.seccion_propuesta_data = res.acf.seccion_propuesta;
+        this.titulo_pilares_data = res.acf.titulo_pilares;
         this.pilar_data = res.acf.pilar;
         this.mapa_procesos_data = res.acf.mapa_procesos;
-    
+        this.quienes_somos = res.acf.quienes_somos;
       });
   }
-
 }
