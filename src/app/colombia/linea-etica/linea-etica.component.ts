c264e64ec662a3e50f96fa16ca8de9efb72b18d6
Nelson Zambrano <inge5neuromedia@gmail.com>
2024-02-12 20:20:35 -0500

    creación de la interna linea etica

--- src/app/colombia/linea-etica/linea-etica.component.ts
+++ src/app/colombia/linea-etica/linea-etica.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+
+declare var $: any;
 
 @Component({
   selector: 'app-linea-etica',
   templateUrl: './linea-etica.component.html',
-  styleUrls: ['./linea-etica.component.css']
+  styleUrls: ['./linea-etica.component.css'],
 })
 export class LineaEticaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  formulario: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.crearForm();
   }
 
+  ngOnInit(): void {}
+
+  crearForm() {
+    this.formulario = this.fb.group({
+      rol: ['Colaborador', [Validators.required]],
+      lugar: ['Colombia', [Validators.required]],
+      denuncia: ['', [Validators.required]],
+      acepto: [false, [Validators.requiredTrue]],
+    });
+  }
+
+  enviarForm() {
+    if (this.formulario.invalid) return;
+    $.ajax({
+      url: `${environment.domain}/wp-content/themes/gers/formulario-linea-etica/form-linea-etica.php`,
+      type: 'POST',
+      data: JSON.stringify(this.formulario.value),
+      dataType: 'json',
+      success: function (data) {},
+      error: function (error) {
+        if (error.status === 200) {
+          Swal.fire({
+            icon: 'success',
+            title:
+              'Gracias por regalarnos tus datos. Nos comunicaremos contigo.',
+            showConfirmButton: true,
+          });
+        } else {
+          Swal.fire(
+            'Oops...',
+            'Algo pasó. Corrige los errores, por favor!',
+            'error'
+          );
+        }
+      },
+    });
+  }
 }
